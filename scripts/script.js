df7d741872dfda0cc06a536d7fd70c49b7851d23
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-12-14 16:18:50 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const botaoVerificar = document.querySelector("#botao-verificar");
+const palavraInput = document.querySelector("#palavra");
+const resultado = document.querySelector("#resultado");
+
+function verificarPalindromo() {
+    const palavra = palavraInput.value;
+    const palavraInvertida = palavra.split("").reverse().join("");
+
+    if (palavra.toLowerCase() === palavraInvertida.toLowerCase()) {
+        resultado.textContent = `A palavra "${palavra}" é um palindromo.`;
+    } else {
+        resultado.textContent = `A palavra "${palavra}" não é um palindromo.`;
+    }
+}
+
+botaoVerificar.addEventListener("click", verificarPalindromo);
+
+palavraInput.addEventListener("keyup", (e) => {
+    
+    if (e.key === "Enter") {
+        e.preventDefault();
+        verificarPalindromo();
+    }
+})
